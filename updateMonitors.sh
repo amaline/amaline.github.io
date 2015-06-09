902fe84eb5c61dce365176f42c41bfa8a3e60e12
Al Maline <amaline@users.noreply.github.com>
2015-06-09 10:47:20 -0400

    Update updateMonitors.sh

--- updateMonitors.sh
+++ updateMonitors.sh
@@ -1,3 +1,5 @@
-#!/bin/bash
-echo KEY=${UPTIME_ROBOT_API_KEY}
-curl -i -H 'Accept: application/json' -H 'Content-Type: application/json' -X GET https://api.uptimerobot.com/getMonitors?apiKey=${UPTIME_ROBOT_API_KEY}&format=json
+#!/bin/bash -xv
+echo Running $0
+URL="https://api.uptimerobot.com/getMonitors?apiKey=${UPTIME_ROBOT_API_KEY}&format=json"
+echo URL=$URL
+curl -i -H 'Accept: application/json' -H 'Content-Type: application/json' -X GET $URL
